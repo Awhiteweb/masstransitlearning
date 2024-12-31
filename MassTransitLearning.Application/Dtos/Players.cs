679dbc7b591f3faa1fb10145c0d66ca0bd19deb8
Alex White <alex_p_white@yahoo.co.uk>
2024-12-31 19:39:26 +0000

    added players consts and updated consumers

---
+++ MassTransitLearning.Application/Dtos/Players.cs
@@ -0,0 +1,11 @@
+namespace MassTransitLearning.Application.Dtos
+{
+    public static class Players
+    {
+        public const string PlayerOne = "Player one";
+        public const string PlayerTwo = "Player two";
+        public const string PlayerThree = "Player three";
+        public const string PlayerFour = "Player four";
+        public const string PlayerFive = "Player five";
+    }
+}
